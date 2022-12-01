a556c0aee4869c085b4d613d24ed4d829bc6e9e3
anastasia <Anastasia9595@googlemail.com>
2022-12-01 19:43:25 +0100

    refector function to change units

--- src/App.js
+++ src/App.js
@@ -8,19 +8,18 @@ function App() {
 
   const [error, setError] = useState(false);
   const [celsius, setCelsius] = useState(false);
-  const [units, setUnits] = useState("metric");
   const [temp, setTemp] = useState(0.0);
 
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=${units}&appid=d727e50d61ad9921e0d23e3146ce3d37`;
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=d727e50d61ad9921e0d23e3146ce3d37`;
   const searchLocation = (event) => {
     if (event.key === "Enter") {
       axios
         .get(url)
         .then((res) => {
-          setTemp(res.data.main.temp);
           setData(res.data);
-          console.log(res.data);
-          // setLocation("");
+          setTemp(res.data.main.temp);
+          console.log(temp);
+          //setLocation("");
         })
         .catch((err) => {
           console.log(err);
@@ -31,27 +30,9 @@ function App() {
   };
 
   const switchToggle = () => {
-    getTemp();
     setCelsius(!celsius);
-    unitBuilder();
   };
 
-  const unitBuilder = () => {
-    if (celsius === false) {
-      setUnits("metric");
-    } else {
-      setUnits("imperial");
-    }
-    console.log(units);
-  };
-
-  //get temp data
-  const getTemp = () => {
-    axios.get(url).then((res) => {
-      setTemp(res.data.main.temp);
-      console.log(temp);
-    });
-  };
   return (
     <div className="app">
       <div className="search">
@@ -84,7 +65,7 @@ function App() {
               ) : (
                 <div className="temp">
                   {" "}
-                  {data.main && <h1>{Math.round(temp)}°F</h1>}
+                  {data.main && <h1>{Math.round(temp * 1.8 + 32)}°F</h1>}
                 </div>
               )}
 
